a0ec9b8b551546a21d5f24fd05c4630132eaf713
gitforwork <gitforwork@outlook.com>
2018-12-26 22:26:40 -0500

    try auto format

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ function readDb() {
     console.dir(results);
 }
 
-function readDb2(){
+function readDb2() {
     //let's see if the file is auto saved and formatted
     mssqlAdapter.queryWithPromise('select top 1 * from dbo.H1B')
         .then(result => console.log(result))
         .catch(err => console.error(err));
+}
+
+function hell() { 
+    return 'hellp';
 }
 
 readDb2();
